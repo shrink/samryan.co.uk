e7c83ae03f0553cb9fcdab97111bbeb8440fbbd9
Samuel Ryan <sam@samryan.co.uk>
2013-08-01 03:35:29 +0100

    added CNAMER

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 require 'vendor/autoload.php';
 require 'helper.php';
 
-respond('/', function($request, $response) {
+$klein = new \Klein\Klein();
+
+$klein->respond('GET', '/', function ($request, $response, $service) {
     $projects = json_decode(file_get_contents("projects.json"), true);
     $projects = $projects["projects"]; // php 5.3... cba to risk trying to update my main web server and break everything lolol
     
     if(empty($projects))
         die("validate the project list"); // :|
     
-    $response->render('html/index.phtml', array("projects" => $projects));
+    $service->render('html/index.phtml', array("projects" => $projects));
 });
 
-respond('404', function($request, $response) {
+$klein->respond('GET', '404', function ($request, $response, $service) {
     $response->header("Content-Type", "application/json");
     $response->code(404);
     echo json_encode(array("404" => "Page not found", "More oh more" => "go to samryan.co.uk"));
 });
 
-dispatch();
+$klein->dispatch();
