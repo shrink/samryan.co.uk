42368a1b10daf3b3c53ede89affc429d033fc53b
Samuel Ryan <sam@samryan.co.uk>
2016-04-21 02:21:18 +0100

    Fixes typo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 build="build-${CIRCLE_BUILD_NUM}"
 sudo mkdir ~/${build}
-sudo cp -R ~/{MY_REPOSITORY}/build/. ~/${build}
+sudo cp -R ~/${MY_REPOSITORY}/build/. ~/${build}
 echo "Deploying ${build} to ${DEPLOY_TARGET}"
 rsync -r -l ~/${build} ${DEPLOY_TARGET}:${MY_DIRECTORY}
 ssh ${DEPLOY_TARGET} "ln -sfn ${MY_DIRECTORY}/${build} ${MY_DIRECTORY}/live"
