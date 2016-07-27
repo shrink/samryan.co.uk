a35038b634d5e066e73b3efa6064d9d8f6310721
Samuel Ryan <sam@samryan.co.uk>
2016-07-27 15:44:35 +0100

    Quotes variables to cope with possible spaces
    Thanks @kamaln7 :+1:

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ sudo mkdir ~/${build}
 sudo cp -R ~/${MY_REPOSITORY}/build/. ~/${build}
 
 echo "Deploying ${build} to ${DEPLOY_TARGET}"
-rsync -r -l ~/${build} ${DEPLOY_TARGET}:${MY_DIRECTORY}
-ssh ${DEPLOY_TARGET} "ln -sfn ${MY_DIRECTORY}/${build} ${MY_DIRECTORY}/live"
+rsync -r -l "~/${build}" "${DEPLOY_TARGET}:${MY_DIRECTORY}"
+ssh ${DEPLOY_TARGET} "ln -sfn \"${MY_DIRECTORY}/${build}\" \"${MY_DIRECTORY}/live\""
 echo "Build deployed and pushed live"
 
 aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths /*
+aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*"
 echo "Cloudfront invalidation submitted, invalidation will take up to 15 minutes"
