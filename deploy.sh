d700759e1b73d305f4ce50d08615d103761e354a
Samuel Ryan <sam@samryan.co.uk>
2016-07-27 15:55:49 +0100

    Fixes missing closing quote

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ sudo cp -R ~/${MY_REPOSITORY}/build/. ~/${build}
 
 echo "Deploying ${build} to ${DEPLOY_TARGET}"
 rsync -r -l ~/${build} ${DEPLOY_TARGET}:${MY_DIRECTORY}
-ssh ${DEPLOY_TARGET} "ln -sfn ${MY_DIRECTORY}/${build} ${MY_DIRECTORY}/live
+ssh ${DEPLOY_TARGET} "ln -sfn ${MY_DIRECTORY}/${build} ${MY_DIRECTORY}/live"
 echo "Build deployed and pushed live"
 
 aws configure set preview.cloudfront true
