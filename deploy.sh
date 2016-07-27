cab8ea8a29824388a5e29657969cd047af6c554f
Samuel Ryan <sam@samryan.co.uk>
2016-07-27 15:54:16 +0100

    unquotes variables because they broke, woops

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ sudo mkdir ~/${build}
 sudo cp -R ~/${MY_REPOSITORY}/build/. ~/${build}
 
 echo "Deploying ${build} to ${DEPLOY_TARGET}"
-rsync -r -l "~/${build}" "${DEPLOY_TARGET}:${MY_DIRECTORY}"
-ssh ${DEPLOY_TARGET} "ln -sfn \"${MY_DIRECTORY}/${build}\" \"${MY_DIRECTORY}/live\""
+rsync -r -l ~/${build} ${DEPLOY_TARGET}:${MY_DIRECTORY}
+ssh ${DEPLOY_TARGET} "ln -sfn ${MY_DIRECTORY}/${build} ${MY_DIRECTORY}/live
 echo "Build deployed and pushed live"
 
 aws configure set preview.cloudfront true
